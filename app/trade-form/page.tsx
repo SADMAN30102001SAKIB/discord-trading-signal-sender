84bd428287ddd34ddd8f4dc184a5d47530edd483
SADMAN30102001SAKIB <evan30102001@gmail.com>
2024-12-27 15:36:26 +0000

    v4.4.1

--- app/trade-form/page.tsx
+++ app/trade-form/page.tsx
@@ -287,9 +287,6 @@ const TradeForm = () => {
   return (
     <div className="bg-[#36393f] min-h-screen flex justify-center items-center">
       <div className="bg-[#2f3136] p-6 rounded-lg shadow-md w-full max-w-sm border border-[#202225]">
-        {/* <h1 className="text-2xl font-bold text-center text-white mb-6">
-          🚀 Send Trading Signal
-        </h1> */}
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block text-[#b9bbbe] font-medium mb-1">
